0d7c893103ac4f32ed8983d7d62e048e9c753036
Jeganathan Swaminathan <jegan@tektutor.org>
2024-07-12 15:55:11 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,19 +1,17 @@
 terraform {
-  required_version = ">=0.12"
-
   required_providers {
     azurerm = {
         source = "hashicorp/azurerm"
-        version = "~>2.0"
+        version = "3.112.0"
     }
     random = {
         source = "hashicorp/random"
-        version = "~>3.0"
+        version = "3.6.2"
     }
 
     tls = {
         source = "hashicorp/tls"
-        version = "~>4.0"
+        version = "4.0.5"
     }
  }
 }
